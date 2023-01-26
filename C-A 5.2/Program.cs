971c6a742d475e1d862af11bda38484d851bbb33
Pingvu <nhlpool27@gmail.com>
2023-01-26 13:56:42 +0100

    Add project files.

---
+++ C-A 5.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hur många tal vill du skriva in?");
+
+int[] HT = new int[Console.ReadLine().Length];
+
+Console.WriteLine("Skriv in 3 heltal");
+for (int i = 0; i < HT.Length; i++)
+{
+    HT[i] = Console.ReadLine().Length;
+}
+{
+    Console.WriteLine("Du skrev in " + HT.Length);
+}
